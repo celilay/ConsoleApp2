b576bb5ec0428fe06be883e87d768ac563df551e
celil <celil@DESKTOP-NCRMDVP>
2023-03-25 11:49:46 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,14 @@
+﻿using Matematik;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(5, 6);
+        dortIslem.topla(7, 2);
+
+
+    }
+}
